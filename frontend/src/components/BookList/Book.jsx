c176a2bb2228018d384c45812ca7504d0abf8e68
nikolas <nikola.stojilkovic6@gmail.com>
2023-02-06 20:46:20 +0100

    no image image

--- frontend/src/components/BookList/Book.jsx
+++ frontend/src/components/BookList/Book.jsx
@@ -11,7 +11,13 @@ const Book = ({ book }) => {
 
   function image() {
     if (book.image == null) {
-      return <></>
+      return (
+        <img
+          src={
+            "https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg"
+          }
+        ></img>
+      )
     } else {
       return <img src={`http://127.0.0.1:8000${book.image.url}`}></img>
     }
